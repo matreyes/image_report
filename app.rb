fc01a1917e09c56608a00ffbcd1b36b4669db171
Matías Reyes <matreyes@gmail.com>
2019-06-11 19:43:56 -0400

    fixes

--- app.rb
+++ app.rb
@@ -4,24 +4,25 @@ require 'date'
 
 files_path = ARGV[0] || "example"
 
-
 @report = YAML.load(File.read("#{files_path}/informe.yaml", encoding: "UTF-8"))
 
 @report["desc"] = @report["desc"].gsub(/^(.+)\:/, '\textbf{\1}:')
 
 # clean special chars from image filenames
 Dir["#{files_path}/*.jpg"].each do |i|
-  File.rename(i, i.gsub(/([\.\_\s]|jpg)/, "")+".jpg")
+  path = File.dirname(i)
+  filename = File.basename(i, ".*").gsub(/[\.\_\s]/, "")
+  ext = File.extname(i)
+  File.rename(i, path+"/"+filename+ext)
 end
 
-@images = Dir["#{files_path}/*.jpg"].each_slice(2).to_a
+@images = Dir["#{files_path}/*.jpg"].map{ |r| File.basename(r) }.each_slice(2).to_a
 
 template = File.read("./template.tex", encoding: "UTF-8")
 
 result = ERB.new(template).result(binding)
 latex_file = "#{@report["paciente"]}_#{Date.today}"
 File.write("#{files_path}/#{latex_file}.tex", result)
+puts files_path
 
-`pdflatex -output-directory #{files_path} #{files_path}/#{latex_file}.tex`
-
-`open #{files_path}/#{latex_file}.pdf`
+`cd '#{files_path}' && pdflatex #{latex_file}.tex && open #{latex_file}.pdf`
